8515c3516f1562a7cf8ab66d8dbcf6b1e661f41b
Alessandro Astone <ales.astone@gmail.com>
2021-01-30 00:55:01 +0100

    Build pixel touch HAL
    Change-Id: I45cff81e1159613094440dd7b2b73b53519c9de6

--- BoardConfigLineage.mk
+++ BoardConfigLineage.mk
@@ -5,6 +5,7 @@
 #
 
 # Manifests
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/calyx/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += device/google/gs201/android.hardware.security.rkp-service.citadel.xml
 ifneq ($(BOARD_WITHOUT_RADIO),true)
 DEVICE_MANIFEST_FILE += device/google/gs201/manifest_radio_ds.xml
