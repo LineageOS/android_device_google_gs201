6a4af337166b88f7d205c113ff7dbd6fd6ee277f
Michael Bestas <mkbestas@lineageos.org>
2023-05-20 22:58:22 +0300

    Build Lineage Health HAL
    Change-Id: I70cc903a03ac9fa3303bb8fa039c9de4806449bf

--- lineage_common.mk
+++ lineage_common.mk
@@ -12,6 +12,9 @@ PRODUCT_PRODUCT_PROPERTIES += \
     ro.vendor.camera.extensions.package=com.google.android.apps.camera.services \
     ro.vendor.camera.extensions.service=com.google.android.apps.camera.services.extensions.service.PixelExtensions
 
+# Lineage Health
+include hardware/google/pixel/lineage_health/device.mk
+
 # Linker config
 PRODUCT_VENDOR_LINKER_CONFIG_FRAGMENTS += \
     device/google/gs201/linker.config.json
