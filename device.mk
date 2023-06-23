847b6c441d44305e2fa799c780892cff79e7622c
LuK1337 <priv.luk@gmail.com>
2023-06-23 19:01:32 +0200

    gs201: add 'tablet' to product characteristics when needed
    Change-Id: I20105565a1b313fe6ff8499b49d3691f776f9267

--- device.mk
+++ device.mk
@@ -728,7 +728,11 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += vendor.hwc.dpp.downscale=2
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.vendor.ddk.set.afbc=1
 
+ifeq ($(USE_TABLET_BT_COD),true)
+PRODUCT_CHARACTERISTICS := nosdcard,tablet
+else
 PRODUCT_CHARACTERISTICS := nosdcard
+endif
 
 # WIFI COEX
 PRODUCT_COPY_FILES += \
