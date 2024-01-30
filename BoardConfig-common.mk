55257cbde8b3e9f223911aa06a143873711cd64a
Joe Onorato <joeo@google.com>
2024-01-29 18:49:40 -0800

    remove build log spam
    Test: m
    Change-Id: I5bd60de66b31c7c5df43240161350d0f5d4da29f

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -416,9 +416,6 @@ BOARD_VENDOR_KERNEL_MODULES := $(KERNEL_MODULES)
 endif
 
 BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(KERNEL_MODULE_DIR)/system_dlkm.modules.load))
-ifndef BOARD_SYSTEM_KERNEL_MODULES_LOAD
-$(warning system_dlkm.modules.load not found or empty)
-endif
 BOARD_SYSTEM_KERNEL_MODULES := $(addprefix $(KERNEL_MODULE_DIR)/, $(notdir $(BOARD_SYSTEM_KERNEL_MODULES_LOAD)))
 
 # Using BUILD_COPY_HEADERS
