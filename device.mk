0e6bc4fd5c6d828e7f8a2ddaad8951d442475cee
Richard Hsu <hsuy@google.com>
2024-02-05 15:06:55 -0800

    [Refactor] Include edgetpu targets in final image
    Make the EdgeTPU targets defined in <soc>/proprietary visible by product packages included in edgetpu.mk.
    
    Bug: 323601370
    
    Tested:
    No functionality change. Edgetpu services still work.
    
    Change-Id: Ib3835ee3976dcfa86df999d0a32398bf8ec9a2cb

--- device.mk
+++ device.mk
@@ -1065,6 +1065,14 @@ PRODUCT_PROPERTY_OVERRIDES += persist.vendor.enable.thermal.genl=true
 include device/google/gs-common/edgetpu/edgetpu.mk
 # Config variables for TPU chip on device.
 $(call soong_config_set,edgetpu_config,chip,janeiro)
+# Include the edgetpu targets defined the namespaces below into the final image.
+PRODUCT_SOONG_NAMESPACES += \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/metrics \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/tflite_delegate \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/darwinn_logging_service \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/nnapi_stable_aidl \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/aidl \
+	vendor/google_devices/gs201/proprietary/gchips/tpu/hal
 # TPU firmware
 PRODUCT_PACKAGES += edgetpu-janeiro.fw
 
