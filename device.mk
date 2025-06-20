ba3f8a0432fd79b09e2a86230cc262ecca3aacd2
Michael Bestas <mkbestas@gmail.com>
2025-06-20 18:46:12 +0300

    gs201: Move wfc-pkt-router to gs-common
    Change-Id: I922b28e1bd8dc0baece89428c0032eabde5c13dc

--- device.mk
+++ device.mk
@@ -973,7 +973,7 @@ PRODUCT_PACKAGES += \
 	libgc2_utils
 
 ## Start packet router
-include device/google/gs101/telephony/pktrouter.mk
+include device/google/gs-common/telephony/pktrouter.mk
 
 # Thermal HAL
 PRODUCT_PROPERTY_OVERRIDES += persist.vendor.enable.thermal.genl=true
