74384c078765fc34c22933bc1bf2dca06fbf5978
Michael Bestas <mkbestas@gmail.com>
2025-06-22 03:25:55 +0300

    gs201: Remove SystemUI dexpreopt speed compile flags
    They are already defined in vendor
    
    Change-Id: I27097e32091d0066fc3da1a1b786e5a6ae11bc86

--- device.mk
+++ device.mk
@@ -1053,21 +1053,6 @@ include device/google/gs201/dumpstate/item.mk
 # It must be split into the FCM of each partition.
 DEVICE_PRODUCT_COMPATIBILITY_MATRIX_FILE += device/google/gs201/device_framework_matrix_product.xml
 
-# Preopt SystemUI
-ifneq ($(RELEASE_SYSTEMUI_USE_SPEED_PROFILE), true)
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUIGoogle  # For internal
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI        # For AOSP
-endif
-
-# Set on-device compilation mode for SystemUI.
-ifeq ($(RELEASE_SYSTEMUI_USE_SPEED_PROFILE), true)
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed-profile
-else
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.systemuicompilerfilter=speed
-endif
-
 # Keymint configuration
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml \
